15b49e4d7361615101d7be547be7e5f491da295b
supersheep <supersheepxjy@gmail.com>
2012-08-26 20:54:54 +0800

    [style] remove margin bottom

--- application/views/pages/pageerror.php
+++ application/views/pages/pageerror.php
@@ -1,4 +1,4 @@
-<div id="id" style="background-color:#fff;width:960px;margin:20px auto;">
+<div id="id" style="background-color:#fff;width:960px;margin:20px auto 0 auto;">
 	
 	<div style="padding:120px 0;text-align:center;">
 		<?=$msg;?>
